2e377b2079dc3b6b68cd2da00d94ef7bc943417d
Volodymyr Vyshnenko <vyshnenko2904@gmail.com>
2020-10-20 21:27:48 +0300

    Добавьте файлы проекта.

---
+++ 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длинну прямоугольника -> ");
+            string x = Console.ReadLine();
+            double side1 = Int32.Parse(x);
+
+            Console.Write("Введите ширину прямоугольника -> ");
+            string y = Console.ReadLine();
+            double side2 = Int32.Parse(y);
+
+            Rectangle rectangle = new Rectangle(side1, side2);
+
+            Console.WriteLine("Площадь = {0}", rectangle.Area);
+            Console.WriteLine("Периметр = {0}", rectangle.Perimeter);
+        }
+    }
+}
---
+++ 2/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿namespace _2
+{
+    internal class Rectangle
+    {
+        private double side1, side2;
+
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+        public Rectangle() { }
+
+        double AreaCalculator()
+        {
+            return side1 * side2;
+        }
+
+        double PerimeterCalculator()
+        {
+            return (side1 + side2) * 2;
+        }
+
+        public double Area { get { return this.AreaCalculator(); } }
+        public double Perimeter { get { return this.PerimeterCalculator(); } }
+    }
+}
